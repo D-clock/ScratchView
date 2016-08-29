bddc27b45f2d055180deb215daf2ebcf7d2586f1
clock <1084991652clock@gmail.com>
2016-08-29 19:19:52 +0800

    新增Scratch蒙层和水印效果

--- Scratch/src/main/java/com/clock/scratch/ScratchView.java
+++ Scratch/src/main/java/com/clock/scratch/ScratchView.java
@@ -2,11 +2,16 @@ package com.clock.scratch;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 
 /**
@@ -15,6 +20,27 @@ import android.view.View;
 public class ScratchView extends View {
 
     private final static String TAG = ScratchView.class.getSimpleName();
+
+    /**
+     * 遮罩 Paint
+     */
+    private Paint mMaskPaint;
+    /**
+     * 产生遮罩效果的 Bitmap
+     */
+    private Bitmap mMaskBitmap;
+    /**
+     * 绘制遮罩的 Canvas
+     */
+    private Canvas mMaskCanvas;
+    /**
+     * 普通绘制 Bitmap 用的 Paint
+     */
+    private Paint mBitmapPaint;
+    /**
+     * 水印
+     */
+    private BitmapDrawable mWatermark;
 
     public ScratchView(Context context) {
         super(context);
@@ -38,22 +64,68 @@ public class ScratchView extends View {
     }
 
     private void init() {
+        mMaskPaint = new Paint();
+        mMaskPaint.setAntiAlias(true);
+        mMaskPaint.setDither(true);
+        setMaskColor(0xFFCCCCCC);
 
+        mBitmapPaint = new Paint();
+        mBitmapPaint.setAntiAlias(true);
+        mBitmapPaint.setDither(true);
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.veins);
+        mWatermark = new BitmapDrawable(bitmap);
+        mWatermark.setTileModeXY(Shader.TileMode.REPEAT, Shader.TileMode.REPEAT);
+    }
+
+    /**
+     * 设置蒙板颜色
+     *
+     * @param color 十六进制颜色值，如：0xFFFF0000（不透明的红色）
+     */
+    public void setMaskColor(int color) {
+        mMaskPaint.setColor(color);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int measuredWidth = measureSize(widthMeasureSpec);
+        int measuredHeight = measureSize(heightMeasureSpec);
+        setMeasuredDimension(measuredWidth, measuredHeight);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        Log.i(TAG, "onDraw");
         super.onDraw(canvas);
-        Paint paint = new Paint();
-        paint.setColor(0xFF880000);
-        paint.setStyle(Paint.Style.STROKE);
-        canvas.drawRect(0, 0, canvas.getWidth(), canvas.getHeight(), paint);
+        canvas.drawBitmap(mMaskBitmap, 0, 0, mBitmapPaint);//绘制图层遮罩
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        Log.i(TAG, "onSizeChanged");
         super.onSizeChanged(w, h, oldw, oldh);
+
+        mMaskBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        mMaskCanvas = new Canvas(mMaskBitmap);
+        Rect rect = new Rect(0, 0, w, h);
+        mMaskCanvas.drawRect(rect, mMaskPaint);//绘制生成和控件大小一致的遮罩 Bitmap
+
+        Rect bounds = new Rect(rect);
+        mWatermark.setBounds(bounds);
+        mWatermark.draw(mMaskCanvas);
+    }
+
+    private int measureSize(int measureSpec) {
+        int size = 0;
+        int specMode = MeasureSpec.getMode(measureSpec);
+        int specSize = MeasureSpec.getSize(measureSpec);
+        if (specMode == MeasureSpec.EXACTLY) {
+            size = specSize;
+        } else {
+            if (specMode == MeasureSpec.AT_MOST) {
+                size = Math.min(size, specSize);
+            }
+        }
+        return size;
     }
 }
