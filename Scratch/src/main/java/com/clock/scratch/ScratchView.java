61a446464df64afb616d8f689bb351184148982c
clock <1084991652clock@gmail.com>
2016-08-30 17:35:24 +0800

    完善代码设计

--- Scratch/src/main/java/com/clock/scratch/ScratchView.java
+++ Scratch/src/main/java/com/clock/scratch/ScratchView.java
@@ -6,12 +6,16 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
 import android.graphics.Shader;
 import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -20,6 +24,14 @@ import android.view.View;
 public class ScratchView extends View {
 
     private final static String TAG = ScratchView.class.getSimpleName();
+    /**
+     * 最小的橡皮擦尺寸大小
+     */
+    private final static float DEFAULT_ERASER_SIZE = 60f;
+    /**
+     * 默认蒙板的颜色
+     */
+    private final static int DEFAULT_MASKER_COLOR = 0xffcccccc;
 
     /**
      * 遮罩 Paint
@@ -41,6 +53,14 @@ public class ScratchView extends View {
      * 水印
      */
     private BitmapDrawable mWatermark;
+    /**
+     * 橡皮檫画笔
+     */
+    private Paint mErasePaint;
+    /**
+     * 擦除轨迹
+     */
+    private Path mErasePath;
 
     public ScratchView(Context context) {
         super(context);
@@ -65,9 +85,9 @@ public class ScratchView extends View {
 
     private void init() {
         mMaskPaint = new Paint();
-        mMaskPaint.setAntiAlias(true);
-        mMaskPaint.setDither(true);
-        setMaskColor(0xFFCCCCCC);
+        mMaskPaint.setAntiAlias(true);//抗锯齿
+        mMaskPaint.setDither(true);//防抖
+        setMaskColor(DEFAULT_MASKER_COLOR);
 
         mBitmapPaint = new Paint();
         mBitmapPaint.setAntiAlias(true);
@@ -76,12 +96,31 @@ public class ScratchView extends View {
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.veins);
         mWatermark = new BitmapDrawable(bitmap);
         mWatermark.setTileModeXY(Shader.TileMode.REPEAT, Shader.TileMode.REPEAT);
+
+        mErasePaint = new Paint();
+        mErasePaint.setAntiAlias(true);
+        mErasePaint.setDither(true);
+        mErasePaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));//设置擦除效果
+        mErasePaint.setStyle(Paint.Style.STROKE);
+        mErasePaint.setStrokeCap(Paint.Cap.ROUND);//设置笔尖形状，让绘制的边缘圆滑
+        setEraserSize(DEFAULT_ERASER_SIZE);
+
+        mErasePath = new Path();
+    }
+
+    /**
+     * 设置橡皮檫尺寸大小（默认大小是 60）
+     *
+     * @param eraserSize 橡皮檫尺寸大小
+     */
+    public void setEraserSize(float eraserSize) {
+        mErasePaint.setStrokeWidth(eraserSize);
     }
 
     /**
      * 设置蒙板颜色
      *
-     * @param color 十六进制颜色值，如：0xFFFF0000（不透明的红色）
+     * @param color 十六进制颜色值，如：0xffff0000（不透明的红色）
      */
     public void setMaskColor(int color) {
         mMaskPaint.setColor(color);
@@ -99,6 +138,29 @@ public class ScratchView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         canvas.drawBitmap(mMaskBitmap, 0, 0, mBitmapPaint);//绘制图层遮罩
+        canvas.drawPath(mErasePath, mErasePaint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int action = event.getAction();
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                startErase();
+                invalidate();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                erase();
+                invalidate();
+                break;
+            case MotionEvent.ACTION_UP:
+                stopErase();
+                invalidate();
+                break;
+            default:
+                break;
+        }
+        return true;
     }
 
     @Override
@@ -127,5 +189,26 @@ public class ScratchView extends View {
             }
         }
         return size;
+    }
+
+    /**
+     * 开始擦除
+     */
+    private void startErase() {
+
+    }
+
+    /**
+     * 擦除
+     */
+    private void erase() {
+
+    }
+
+    /**
+     * 停止擦除
+     */
+    private void stopErase() {
+
     }
 }
